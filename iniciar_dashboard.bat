6c7e7bacf9c6c972b47088ff99030644c523ad08
Alex Biudes de Souza Filho <alex.biudes@Planning.local>
2026-06-03 12:29:59 -0300

    fix: adiciona cd para garantir execucao do bat em qualquer contexto e resolve processos travados

--- iniciar_dashboard.bat
+++ iniciar_dashboard.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 echo ========================================================
 echo        Iniciando o Planning BI - Dashboards Locais
 echo ========================================================
